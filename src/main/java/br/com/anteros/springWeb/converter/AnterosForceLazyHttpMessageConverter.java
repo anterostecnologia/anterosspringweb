1e5e8b88c424cbf307128630185f536371984c4c
Edson Martins <edsonmartins2005@gmail.com>
2014-09-25 10:00:58 -0300

    Corrigido pom.xml

--- src/main/java/br/com/anteros/springWeb/converter/AnterosForceLazyHttpMessageConverter.java
+++ src/main/java/br/com/anteros/springWeb/converter/AnterosForceLazyHttpMessageConverter.java
@@ -1,18 +1,33 @@
 package br.com.anteros.springWeb.converter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 import br.com.anteros.persistence.serialization.jackson.AnterosObjectMapper;
 import br.com.anteros.persistence.serialization.jackson.AnterosPersistenceJacksonModule.Feature;
+import br.com.anteros.persistence.session.SQLSessionFactory;
 
 @Component
 public class AnterosForceLazyHttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
-	public AnterosForceLazyHttpMessageConverter() {
-		AnterosObjectMapper mapper = new AnterosObjectMapper();
+	private SQLSessionFactory sessionFactory;
+
+	public AnterosForceLazyHttpMessageConverter(SQLSessionFactory sessionFactory) {
+		this.setSessionFactory(sessionFactory);
+	}
+
+	@Autowired
+	public void setSessionFactory(SQLSessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+		AnterosObjectMapper mapper = new AnterosObjectMapper(sessionFactory);
 		mapper.enable(Feature.FORCE_LAZY_LOADING);
 		this.setObjectMapper(mapper);
 	}
+
+	public SQLSessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
 
 }
--- src/main/java/br/com/anteros/springWeb/converter/AnterosHttpMessageConverter.java
+++ src/main/java/br/com/anteros/springWeb/converter/AnterosHttpMessageConverter.java
@@ -1,15 +1,32 @@
 package br.com.anteros.springWeb.converter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 import br.com.anteros.persistence.serialization.jackson.AnterosObjectMapper;
+import br.com.anteros.persistence.session.SQLSessionFactory;
 
 @Component
 public class AnterosHttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
+	private SQLSessionFactory sessionFactory;
+
 	public AnterosHttpMessageConverter() {
-		this.setObjectMapper(new AnterosObjectMapper());
+	}
+
+	public AnterosHttpMessageConverter(SQLSessionFactory sessionFactory) {
+		this.setSessionFactory(sessionFactory);
+	}
+
+	@Autowired
+	public void setSessionFactory(SQLSessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+		this.setObjectMapper(new AnterosObjectMapper(sessionFactory));
+	}
+
+	public SQLSessionFactory getSessionFactory() {
+		return sessionFactory;
 	}
 
 }
