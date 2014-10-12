5b759c28ad955d4ddbed5a8e3e25427ec6e92d33
Edson Martins <edsonmartins2005@gmail.com>
2014-10-12 11:25:36 -0300

    Correção na tradução.

--- src/main/java/br/com/anteros/springWeb/util/AnterosSpringWebTranslate.java
+++ src/main/java/br/com/anteros/springWeb/util/AnterosSpringWebTranslate.java
@@ -4,16 +4,13 @@ import br.com.anteros.core.utils.AbstractCoreTranslate;
 
 public class AnterosSpringWebTranslate extends AbstractCoreTranslate {
 
-	public AnterosSpringWebTranslate(String messageBundleName) {
+	private AnterosSpringWebTranslate(String messageBundleName) {
 		super(messageBundleName);
 	}
-
-	private static AnterosSpringWebTranslate translate;
 	
-	public AnterosSpringWebTranslate getInstance(){
-		if (translate==null){
-			translate = new AnterosSpringWebTranslate("anterosspringweb_messages");
-		}
-		return translate;
+
+	static {
+		setInstance(new AnterosSpringWebTranslate("anterosspringweb_messages"));
 	}
+
 }
