1c2eee5b40a68cadece66dede1297493c7444fd4
Edson Martins <edsonmartins2005@gmail.com>
2016-05-04 11:26:32 -0300

    - Incremento de versão. - Criado classe para facilitar configuração programática do Spring Mvc com Anteros.

---
+++ src/main/java/br/com/anteros/springWeb/config/AnterosSpringMvcConfiguration.java
@@ -0,0 +1,127 @@
+package br.com.anteros.springWeb.config;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import br.com.anteros.persistence.session.SQLSessionFactory;
+import br.com.anteros.spring.transaction.AnterosTransactionManager;
+import br.com.anteros.springWeb.converter.AnterosHttpMessageConverter;
+
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+public abstract class AnterosSpringMvcConfiguration extends WebMvcConfigurerAdapter {
+
+	private static final String DISPATCHER = "dispatcher";
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+
+		if (getDisplayName() != null) {
+			appContext.setDisplayName(getDisplayName());
+		}
+		if (registerFirstConfigurationClasses() != null) {
+			for (Class<?> clz : registerFirstConfigurationClasses()) {
+				appContext.register(clz);
+			}
+		}
+		if (persistenceConfigurationClass() != null) {
+			appContext.register(persistenceConfigurationClass());
+		}
+
+		if (mvcConfigurationClass() != null) {
+			appContext.register(mvcConfigurationClass());
+		}
+
+		if (swaggerConfigurationClass() != null) {
+			appContext.register(swaggerConfigurationClass());
+		}
+
+		if (jsonDocConfigurationClass() != null) {
+			appContext.register(jsonDocConfigurationClass());
+		}
+
+		if (registerLastConfigurationClasses() != null) {
+			for (Class<?> clz : registerLastConfigurationClasses()) {
+				appContext.register(clz);
+			}
+		}
+		appContext.setServletContext(servletContext);
+		servletContext.addListener(new ContextLoaderListener(appContext));
+
+		Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(appContext));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+	}
+
+	public abstract Class<?>[] registerFirstConfigurationClasses();
+
+	public abstract Class<?>[] registerLastConfigurationClasses();
+
+	public abstract Class<?> persistenceConfigurationClass();
+
+	public abstract Class<?> mvcConfigurationClass();
+
+	public abstract Class<?> swaggerConfigurationClass();
+
+	public abstract Class<?> jsonDocConfigurationClass();
+
+	public abstract void addListener(ServletContext servletContext);
+
+	public abstract String getDisplayName();
+
+	@Autowired
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager txManager(DataSource dataSource, SQLSessionFactory sessionFactory) {
+		if (dataSource != null && sessionFactory != null) {
+			AnterosTransactionManager txManager = new AnterosTransactionManager();
+			txManager.setDataSource(dataSource);
+			txManager.setSessionFactory(sessionFactory);
+			return txManager;
+		}
+		return null;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		super.configureMessageConverters(converters);
+		converters.add(new AnterosHttpMessageConverter());
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("jsondoc-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
